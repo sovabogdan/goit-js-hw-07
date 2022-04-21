df8afa9056dba3c3eb8751d93c378c1c3855985c
Bohdan Sova <sova.bogdan.94@gmail.com>
2022-04-21 20:08:05 +0200

    js_07_01

---
+++ js/01-gallery.js
@@ -0,0 +1,47 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+const galleryMurkup = createGalleryItemsMarkup(galleryItems);
+
+galleryEl.insertAdjacentHTML('beforeend', galleryMurkup);
+galleryEl.addEventListener('click', onOpenLargeImg);
+
+function createGalleryItemsMarkup(galleryItems) {
+
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+    }).join('');
+};
+
+function onOpenLargeImg(e) {
+    const isImgEl = e.target.classList.contains('gallery__image');
+
+    if (!isImgEl) {
+        return;
+    }
+    e.preventDefault();
+
+    const largeImgLink = e.target.dataset.source;
+const instance = basicLightbox.create(`
+    
+        <img src = "${largeImgLink}" alt= "${e.target.alt}">
+    
+`)
+
+instance.show()
+    
+}
+
+
