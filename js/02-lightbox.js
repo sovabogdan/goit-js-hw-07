78002a6cc68f93e1865209e618d55ced030d8f1a
Bohdan Sova <sova.bogdan.94@gmail.com>
2022-04-22 23:53:55 +0200

    task_07_02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,31 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+const galleryMurkup = createGalleryItemsMarkup(galleryItems);
+
+galleryEl.insertAdjacentHTML('beforeend', galleryMurkup);
+galleryEl.addEventListener('click', onOpenSlider);
+
+function createGalleryItemsMarkup(galleryItems) {
+
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+    }).join('');
+};
+
+function onOpenSlider(e) {
+    const isImgEl = e.target.classList.contains('gallery__image');
+
+    if (!isImgEl) {
+        return;
+    }
+    e.preventDefault();
+
+    e.target.setAttribute('title', e.target.alt)
+
+    var lightbox = new SimpleLightbox('.gallery a', {});
+}
